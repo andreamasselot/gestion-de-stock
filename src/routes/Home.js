2dd6437ded88a2ebc1fc8dd1d66c20af69345832
Andréa Masselot <andrea.nicolas128@gmail.com>
2023-01-05 14:16:46 +0100

    formulaire

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,27 @@
 const Home = () => {
   return (
     <>
-      <section>
+      <section className="container">
         <div className="product-creation-container">
           <form className="product-creation">
-            <input type="text" name="product-name" id="product-name" />
-            <input type="text" name="product-price" id="product-price" />
+            <label for="product-name">Product name:</label>
+            <input
+              type="text"
+              name="product-name"
+              id="product-name"
+              placeholder="Product name"
+            />
+            <label for="product-price">Product price:</label>
+            <input
+              type="text"
+              name="product-price"
+              id="product-price"
+              placeholder="Product price"
+            />
+            <label for="product-description">Product description:</label>
             <textarea
               name="product-description"
+              placeholder="Describe the product..."
               id="product-description"
               cols="30"
               rows="10"
