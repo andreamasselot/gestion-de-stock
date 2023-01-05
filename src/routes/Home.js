724290e448941177892896836843c10592c2f7c5
Andréa Masselot <andrea.nicolas128@gmail.com>
2023-01-05 13:55:53 +0100

    home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
   return (
     <>
-      <p>HOME</p>
+      <section>
+        <div className="product-creation-container">
+          <form className="product-creation">
+            <input type="text" name="product-name" id="product-name" />
+            <input type="text" name="product-price" id="product-price" />
+            <textarea
+              name="product-description"
+              id="product-description"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </form>
+        </div>
+        <div className="products-tab"></div>
+      </section>
     </>
   );
 };
